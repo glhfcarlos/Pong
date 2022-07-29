64b0884d09dc28a1571b8d8180bef4cf93bb552d
Melissa Gray <61853436+mag535@users.noreply.github.com>
2022-07-29 14:08:50 -0400

    added comment

--- ElementalPong/Assets/Scripts/GameManager.cs
+++ ElementalPong/Assets/Scripts/GameManager.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int secondsLeft;
     public Transform ballPosition;
     public Text player1ScoreText;
     public Text player2ScoreText;
+    public Text timerText;
 
     private PaddleUnit player1Paddle;
     private PaddleUnit player2Paddle;
@@ -41,6 +43,27 @@ public class GameManager : MonoBehaviour
     }
 
     // FIXME: add code to deal with broken ball
+
+    // FIXME: add timer
+
+    /*
+        This should change the scene to the "GameOver" scene and display who won.
+    */
+    public void EndGame(){
+         /*
+            FIXME: 
+                [ ] This should change the scene to the "GameOver" scene and display who won.
+                [ ] There should be options to restart or return to start screen.
+                [ ] Player input maps should be changed to UI for this.
+        */
+        if (player1Paddle.score > player2Paddle.score){
+            timerText.text = "P1 Wins";
+        }else if (player2Paddle.score > player1Paddle.score){
+            timerText.text = "P2 Wins";
+        }else{
+            timerText.text = "Tie";
+        }
+    }
 
     private void ResetRound()
     {
