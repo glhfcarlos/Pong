0c2c9892161b85c16c7a951f3a50b01f32dcbc07
Omar-Quintanilla <102434404+deathstrider941@users.noreply.github.com>
2022-08-02 15:13:12 -0700

    Start menu finished

---
+++ ElementalPong/Assets/test text/ChangeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
