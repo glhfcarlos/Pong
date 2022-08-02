d4c6b449a260e7c76d2e1a2a3f7bbd701c8a716f
Melissa Gray <61853436+mag535@users.noreply.github.com>
2022-08-02 13:05:06 -0400

    fixed paddle movement
    The paddle no longer has a rigidbody. Movement is manipulated through transform.position

--- ElementalPong/Assets/Scripts/PaddleUnit.cs
+++ ElementalPong/Assets/Scripts/PaddleUnit.cs
@@ -8,17 +8,17 @@ public class PaddleUnit : MonoBehaviour
     // for the player manager
     public int playerID;
     public Vector3 startPosition;
+    
+    public int score;
 
     public float defaultSpeed; // default vertical movement
     public float earthSpeed; // for earth power
     public float waterSpeed; // for the water power
     public float direction;
-    public int score;
     public List<Sprite> elementalSprites;
 
     private float currentSpeed;
     private Vector3 currentVelocity;
-    private Rigidbody2D rb;
 
     private PlayerInput playerInput;
     
@@ -29,22 +29,41 @@ public class PaddleUnit : MonoBehaviour
         direction = 1.0f;
         currentSpeed = defaultSpeed;
         currentVelocity = Vector3.zero;
-        rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
     }
 
     void FixedUpdate(){
         // updates the current velocity
-        currentVelocity.y = direction * currentSpeed;
-        // applies force
-        rb.AddForce(currentVelocity);
-        //Debug.Log(currentVelocity);
+        currentVelocity.y = direction * currentSpeed * Time.deltaTime;
+        // added movement to paddle
+        gameObject.transform.position += currentVelocity;
+        Debug.Log(currentVelocity);
+    }
+
+    // collision
+    void OnCollisionEnter2D(Collision2D other){
+        float deflectionOffset = 1.0f;
+        Vector3 newPosition = new Vector3(gameObject.transform.position.x,
+                                            gameObject.transform.position.y,
+                                            gameObject.transform.position.z);
+
+        // if paddle hits top wall, place it just below wall
+        if (other.gameObject.tag == "TopWall"){
+            newPosition.y = other.gameObject.transform.position.y - other.gameObject.transform.localScale.y - deflectionOffset;
+        }
+
+        // if paddle hits bottom wall, place it just below wall
+        if (other.gameObject.tag == "BottomWall"){
+            newPosition.y = other.gameObject.transform.position.y + other.gameObject.transform.localScale.y + deflectionOffset;
+        }
+
+        gameObject.transform.position = newPosition;
     }
 
     // resets position
     public void ResetPosition(){
-        rb.position = startPosition;
-        rb.velocity = Vector3.zero;
+        gameObject.transform.position = startPosition;
+        ResetCurrentVelocity();
     }
 
     // returns the current speed of the paddle
@@ -104,5 +123,4 @@ public class PaddleUnit : MonoBehaviour
     public void ChangeInputMap(string toMap){
         playerInput.SwitchCurrentActionMap(toMap);
     }
-
 }
