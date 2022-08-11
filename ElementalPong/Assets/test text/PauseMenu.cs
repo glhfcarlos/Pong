023c045e29bb0f08e623e829886f990dd6f42876
Bruce J <102434404+deathstrider941@users.noreply.github.com>
2022-08-11 13:58:25 -0700

    Pause Menu

--- ElementalPong/Assets/test text/PauseMenu.cs
+++ ElementalPong/Assets/test text/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject ResumeButton;
     public GameObject HomeButton;
+    public GameObject Paused;
     public static PauseMenu instance;
 
     void Awake()
@@ -23,12 +24,14 @@ public class PauseMenu : MonoBehaviour
     {
         ResumeButton.SetActive(true);
         HomeButton.SetActive(true);
+        Paused.SetActive(true);
         Time.timeScale = 0f;
     }
     public void Resume()
     {
         ResumeButton.SetActive(false);
         HomeButton.SetActive(false);
+        Paused.SetActive(false);
         Time.timeScale = 1f;
     }
     public void Home(int sceneID)
