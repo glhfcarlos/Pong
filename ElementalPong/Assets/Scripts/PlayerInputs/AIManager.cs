80287e0a3c94559eee7a0e0be48c7a538155ce00
Melissa Gray <61853436+mag535@users.noreply.github.com>
2022-11-25 21:09:32 -0500

    simplified script

--- ElementalPong/Assets/Scripts/PlayerInputs/AIManager.cs
+++ ElementalPong/Assets/Scripts/PlayerInputs/AIManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AIManager : MonoBehaviour
 {
-    public GameManager game;
     public GameObject ai;
+
+    private PlayerManager manager;
 
     // Start is called before the first frame update
     void Start()
     {
+        // FIXME: change max player count to 1
+
+        // Allow Game Manager to track AI
+        manager = GetComponent<PlayerManager>();
         PaddleUnit paddle = ai.GetComponent<PaddleUnit>();
-        game.TrackPlayer2(paddle);
+        manager.game.TrackPlayer2(paddle);
     }
 }
